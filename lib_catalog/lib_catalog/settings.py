5183ef59a0594b22dec2ab5ea42e31f5a8d70317
Натиг Нагиев <nagiev2992@mail.ru>
2022-03-10 00:02:38 +0300

    Lesson 3 over compose file

--- lib_catalog/lib_catalog/settings.py
+++ lib_catalog/lib_catalog/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "NAME": "django",
         "USER": "django",
         "PASSWORD": "django",
-        "HOST": "postgresql_db",
+        "HOST": "postgres_db",
         "PORT": "5432",
         "CONN_MAX_AGE": None
     },
